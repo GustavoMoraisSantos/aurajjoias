35102c3670e14a7eb559eeb2dff0519d0253d4b7
Gustavo Morais Lopes dos Santos <79885252+GustavoMoraisSantos@users.noreply.github.com>
2023-07-26 14:57:55 -0300

    feat: test the github connection with hosting

--- wp-content/themes/storepress/archive.php
+++ wp-content/themes/storepress/archive.php
@@ -20,6 +20,7 @@ get_header();
 					
 						<div class="col-lg-12 mb-4">
 							<?php get_template_part('template-parts/content/content','page');	?>
+							<p>oi wash, editei via vscode</p>
 						</div>
 
 					<?php endwhile; ?>
