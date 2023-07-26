632de69c8cbb643ffc17bbb94e6f7be35461a593
Gustavo Morais Lopes dos Santos <79885252+GustavoMoraisSantos@users.noreply.github.com>
2023-07-26 14:34:15 -0300

    updated composer dependencies

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f7a082544858c41bfed4b9234670890434630495',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f7a082544858c41bfed4b9234670890434630495',
+            'dev_requirement' => false,
+        ),
+    ),
+);
